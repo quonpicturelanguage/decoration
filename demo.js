f282f1c4f9b21a3f9e65b105a22be85c91c6f86d
Youwei Zhao <zhao.uv@gmail.com>
2021-04-21 22:16:48 +0800

    CombineLine

--- demo.js
+++ demo.js
@@ -71,14 +71,113 @@ function getSVGWithFilter(size, opt_type, length_filter) {
     return getSVG(size, opt_type);
 }
 
+function getBezierList(length_filter) {
+
+    let size = 1;
+    let opt_type = "curve";
+
+    var { pathlist } = Potrace.getVars();
+
+    function getList(size, opt_type) {
+
+        function path(curve) {
+
+            function LineTo(p0, p2, size) {
+                let p1 = { x: (p0.x + p2.x) / 2, y: (p0.y + p2.y) / 2 };
+                return new Bezier(
+                    p0.x * size, p0.y * size,
+                    p1.x * size, p1.y * size,
+                    p2.x * size, p2.y * size,
+                );
+            }
+
+            function bezier(i) {
+                let list = [new Bezier(
+                    lastPoint.p.x, lastPoint.p.y,
+                    curve.c[i * 3 + 0].x * size, curve.c[i * 3 + 0].y * size,
+                    curve.c[i * 3 + 1].x * size, curve.c[i * 3 + 1].y * size,
+                    curve.c[i * 3 + 2].x * size, curve.c[i * 3 + 2].y * size,
+                )];
+                lastPoint.p = curve.c[i * 3 + 2];
+                return list;
+            }
+
+            function segment(i) {
+                let list = [
+                    LineTo(lastPoint.p, curve.c[i * 3 + 1], size),
+                    LineTo(curve.c[i * 3 + 1], curve.c[i * 3 + 2], size),
+                ];
+                lastPoint.p = curve.c[i * 3 + 2];
+                return list;
+            }
+
+            var n = curve.n, i;
+            var lastPoint = { p: curve.c[(n - 1) * 3 + 2] };
+            var p = [];
+            for (i = 0; i < n; i++) {
+                if (curve.tag[i] === "CURVE") {
+                    p = p.concat(bezier(i));
+                } else if (curve.tag[i] === "CORNER") {
+                    p = p.concat(segment(i));
+                }
+            }
+            return p;
+        }
+
+        var len = pathlist.length, c, i;
+
+        var lines = [];
+
+        for (i = 0; i < len; i++) {
+            if (pathlist[i].len <= length_filter) continue;
+            c = pathlist[i].curve;
+            lines.push(new CombineLine(path(c),pathlist[i].sign=='+'));
+        }
+
+        return lines;
+    }
+    return getList(size, opt_type);
+}
+
+class CombineLine {
+    constructor(BezierList,sign) {
+        this.BezierList = BezierList;
+        this.sign=sign;
+        this.lengths = BezierList.map(v => v.length());
+        this.length = this.lengths.reduce((a, b) => a + b);
+        this.size = BezierList.length;
+    }
+
+    offset(t, d) {
+        if(t<0||t>1){
+            throw 't<0||t>1';
+        }
+        let len = this.length * t;
+        for (let index = 0; index < this.size; index++) {
+            const leni = this.lengths[index];
+            if (len>leni) {
+                len-=leni;
+            } else {
+                return this.BezierList[index].offset(len/leni,d*(this.sign?1:-1));
+            }
+        }
+        throw 'should not happen';
+    }
+}
+
 function main(params) {
     Potrace.loadImageFromUrl("../Potrace.png");
     Potrace.process(function () {
-        let svg = getSVGWithFilter(1, "curve", 10);
+        let length_filter = 10;
+        let svg = getSVGWithFilter(1, "curve", length_filter);
         // console.log(svg);
         document.body.appendChild(Potrace.img);
         document.body.insertAdjacentHTML("beforeend", '<br>' + svg);
         console.log(document.body.children[document.body.children.length - 1]);
+
+        let lines = getBezierList(length_filter);
+        globalThis.lines = lines;
+        console.log(lines);
     });
 }
 
